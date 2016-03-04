87129597c6ef7308902e0169dcb57b3c1c591fef
fredimage2 <fderue@gmail.com>
2016-03-04 16:33:52 +0000

    Rename ArmClamp with unit test ; Arm too confusing

---
+++ src_test/fr/uja/im2ag/patia/syska/controller/ArmClampTest.java
@@ -0,0 +1,30 @@
+package fr.uja.im2ag.patia.syska.controller;
+
+import fr.uja.im2ag.patia.syska.controller.motion.ArmClamp;
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.port.MotorPort;
+
+public class ArmClampTest {
+	
+	static int MOVEMENT_TEST_DURATION = 5000;
+
+	public static void main(String[] args) throws InterruptedException {
+		ArmClamp arm = new ArmClamp(MotorPort.C);
+		LCD.clear();
+		System.out.println("BEGIN CLOSING ARM CLAMP");
+		arm.closeArmMvt();
+		Thread.sleep(MOVEMENT_TEST_DURATION);
+		System.out.println("END CLOSING ARM CLAMP");
+		
+		System.out.println("BEGIN OPENING ARM CLAMP");
+		arm.openArmMvt();
+		Thread.sleep(MOVEMENT_TEST_DURATION);
+		System.out.println("END OPENING ARM CLAMP");
+		
+		System.out.println("STOP ARM CLAMP");
+		arm.stop();
+		LCD.clear();
+		System.out.println("EXIT");
+		System.exit(0);
+	}
+}
